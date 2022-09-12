15885703413f20ea5ce22001f396f4b227477232
Xharles <charlesnzubechukwu000@gmail.com>
2022-09-12 20:02:52 +0100

    home page

---
+++ components/home/GamesList.js
@@ -0,0 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import React from "react";
+import CirclePlus from "../../assets/circle-plus.svg";
+
+const games = [
+  {
+    id: "1",
+    title: "Quiz Time",
+    slug: "chichen-itza",
+    desc: "",
+    shortDesc: `A smart-contract based multi-player quiz game. Rewards are distributed after the game ends.`,
+    img: "https://i.imgur.com/icR8eAo.png",
+    type: "Multi-Player",
+  },
+];
+
+const GamesList = () => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4 justify-center">
+          {games.map((game) => (
+            <Link key={game.id} href={`/games/${game.slug}`}>
+              <div className="xl:w-1/4 max-w-xs md:w-1/2 p-4 cursor-pointer">
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <img
+                    className="h-40 rounded w-full object-cover object-center mb-6"
+                    src={game.img}
+                    alt="content"
+                  />
+                  <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font uppercase">
+                    {game.type}
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    {game.title}
+                  </h2>
+                  <p className="leading-relaxed text-base h-[78px] three-liner">
+                    {game.shortDesc}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          ))}
+          <div className="xl:w-1/4 max-w-xs md:w-1/2 p-4 flex justify-center items-center h-[402px]">
+            <div className="w-52 h-60 border-gray-300 border-2 rounded-lg flex flex-col justify-center items-center ">
+              <CirclePlus className="stroke-gray-300" />
+              <p className="text-gray-300 text-lg">More Coming Soon!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GamesList;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import GamesList from "../components/home/GamesList";
+import Hero from "../components/home/Hero";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return ( 
-    <div>asdfg</div>
-  )
+  return (
+    <>
+      <Hero />
+      <GamesList />
+    </>
+  );
 }
