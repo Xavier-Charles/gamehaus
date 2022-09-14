45e0c4e392eb58797a68989cdf788abd8c697532
Xharles <charlesnzubechukwu000@gmail.com>
2022-09-14 10:49:35 +0100

    new page

---
+++ pages/game/[...slug].js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { GAMES as games } from "../../data/config";
+import WorldCoin from "../../components/WorldCoin";
+
+const Game = () => {
+  const [[gameSlug, gameId], setSlugs] = useState([]);
+  const [gameCode, setGameCode] = useState();
+  const router = useRouter();
+
+  const game = games.find((game) => game.slug === gameSlug);
+
+  const handleJoin = () => {
+    if (gameSlug && gameCode) router.push(`/game/${gameSlug}/${gameCode}`);
+  };
+
+  useEffect(() => {
+    const { slug } = router.query;
+    if (slug) setSlugs(slug);
+  }, [router.query]);
+
+  if (gameId) return <WorldCoin />;
+
+  return (
+    <section className="">
+      <div
+        className="min-h-screen w-screen flex justify-center items-center"
+        style={{ backgroundImage: "url(https://i.imgur.com/rVhe0ke.png)" }}
+      >
+        <div>
+          <div className="relative">
+            <input
+              className="w-80 sm:w-[500px] md:w-[600px] pl-5 lg:pr-24 sm:text-2xl h-16 p-2 bg-white border-2 border-[#ffae00] shadow rounded focus:outline-none"
+              style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}
+              value={gameCode}
+              onChange={(e) => setGameCode(e.target.value)}
+              placeholder="Enter your game code..."
+            />
+            <button
+              type="button"
+              onClick={handleJoin}
+              className="bg-[#ffae00] text-white px-4 py-3 font-medium rounded absolute right-2 top-2"
+            >
+              Join
+            </button>
+          </div>
+          <div className="flex justify-center mt-20">
+            <button
+              className="bg-[#ffae0099] inline-flex py-3 px-5 rounded-lg items-center hover:bg-[#ffae00] focus:outline-none"
+              style={{ boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px" }}
+            >
+              <span className="mx-4 flex items-start flex-col leading-none">
+                <span
+                  className="sm:text-3xl text-[#fff] mb-1"
+                  style={{ fontFamily: "'Silkscreen', cursive" }}
+                >
+                  Start a New Game
+                </span>
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Game;
