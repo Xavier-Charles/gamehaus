4b20602c0ced215a767b002ad12fb22f97581850
elcharitas <jonathanirhodia@gmail.com>
2022-09-20 13:52:10 +0100

    feat: set up multi wallet support

--- api/utils/coinbase.js
+++ api/utils/coinbase.js
@@ -1,42 +1,48 @@
 import { useState } from "react";
 import { getProvider } from "../providers/CBProvider";
 
+export const useWalletAuth = () => {
+    const [account, setAccount] = useState();
+    const [chainId, setChainId] = useState();
+    const [provider, setProvider] = useState();
+    const [error, setError] = useState();
 
-export const useCoinbaseAuth = () => {
-  const [account, setAccount] = useState();
-  const [chainId, setChainId] = useState();
-  const [provider, setProvider] = useState();
-  const [error, setError] = useState();
+    /* Connect wallet */
+    const connectWallet = async (isMetaMask = false) => {
+        const provider = isMetaMask ? window.ethereum : getProvider();
+        try {
+            console.log("CALLED");
+            const provider = getProvider();
+            setProvider(provider);
+            // Get accounts for connected wallet
+            const accounts = await provider.request({
+                method: "eth_requestAccounts",
+            });
+            if (accounts) {
+                setAccount(accounts[0]);
+            }
+            // Get current chain ID for connected wallet
+            const chainId = await provider.request({
+                method: "eth_chainId",
+            });
+            setChainId(Number(chainId));
+        } catch (error) {
+            setError(error);
+        }
+    };
 
-  /* Connect wallet */
-  const CBConnect = async () => {
-    try {
-      console.log("CALLED");
-      const provider = getProvider();
-      setProvider(provider);
-      // Get accounts for connected wallet
-      const accounts = await provider.request({
-        method: "eth_requestAccounts",
-      });
-      if (accounts) {
-        setAccount(accounts[0]);
-      }
-      // Get current chain ID for connected wallet
-      const chainId = await provider.request({
-        method: "eth_chainId",
-      });
-      setChainId(Number(chainId));
-    } catch (error) {
-      setError(error);
-    }
-  };
+    /* Disconnect wallet */
+    const disconnectWallet = () => {
+        setAccount("");
+        setChainId("");
+    };
 
-
-  /* Disconnect wallet */
-  const CBDisconnect = () => {
-    setAccount("");
-    setChainId("");
-  };
-
-  return {account, chainId, error, CBConnect, CBDisconnect, provider}
-}
+    return {
+        account,
+        chainId,
+        error,
+        connectWallet,
+        disconnectWallet,
+        provider,
+    };
+};
